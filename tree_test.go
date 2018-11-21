a5333e09223e9e661aa9c175529e35d0ea2ab278
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-21 16:43:32 +0900

    add test for Tree.CreateMembershipProof()

--- tree_test.go
+++ tree_test.go
@@ -7,6 +7,17 @@ import (
 	"testing"
 )
 
+func newTestTree(t *testing.T) *Tree {
+	tree, err := NewTree(sha256.New(), 3, map[uint64][]byte{
+		0: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		3: []byte{0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return tree
+}
+
 func TestTree(t *testing.T) {
 	type input struct {
 		hasher hash.Hash
@@ -14,7 +25,7 @@ func TestTree(t *testing.T) {
 		leaves map[uint64][]byte
 	}
 	type output struct {
-		rootStr string
+		rootHex string
 		err     error
 	}
 	testCases := []struct {
@@ -22,6 +33,32 @@ func TestTree(t *testing.T) {
 		in   input
 		out  output
 	}{
+		{
+			"failure: too large tree depth",
+			input{
+				sha256.New(),
+				65,
+				nil,
+			},
+			output{
+				"",
+				ErrTooLargeTreeDepth,
+			},
+		},
+		{
+			"failure: too large leaf index",
+			input{
+				sha256.New(),
+				2,
+				map[uint64][]byte{
+					4: nil,
+				},
+			},
+			output{
+				"",
+				ErrTooLargeLeafIndex,
+			},
+		},
 		{
 			"success: default",
 			input{
@@ -49,32 +86,6 @@ func TestTree(t *testing.T) {
 				nil,
 			},
 		},
-		{
-			"failure: too large tree depth",
-			input{
-				sha256.New(),
-				65,
-				nil,
-			},
-			output{
-				"",
-				ErrTooLargeTreeDepth,
-			},
-		},
-		{
-			"failure: too large leaf index",
-			input{
-				sha256.New(),
-				2,
-				map[uint64][]byte{
-					4: nil,
-				},
-			},
-			output{
-				"",
-				ErrTooLargeLeafIndex,
-			},
-		},
 	}
 
 	for _, tc := range testCases {
@@ -86,9 +97,65 @@ func TestTree(t *testing.T) {
 				t.Errorf("expected: %v, actual: %v", out.err, err)
 			}
 			if err == nil {
-				rootStr := hex.EncodeToString(tree.Root())
-				if rootStr != out.rootStr {
-					t.Errorf("expected: %s, actual: %s", out.rootStr, rootStr)
+				rootHex := hex.EncodeToString(tree.Root())
+				if rootHex != out.rootHex {
+					t.Errorf("expected: %s, actual: %s", out.rootHex, rootHex)
+				}
+			}
+		})
+	}
+}
+
+func TestTree_CreateMembershipProof(t *testing.T) {
+	type input struct {
+		index uint64
+	}
+	type output struct {
+		proofHex string
+		err      error
+	}
+	testCases := []struct {
+		name string
+		tree *Tree
+		in   input
+		out  output
+	}{
+		{
+			"failure: too large leaf index",
+			newTestTree(t),
+			input{
+				8,
+			},
+			output{
+				"",
+				ErrTooLargeLeafIndex,
+			},
+		},
+		{
+			"success: inclusion",
+			newTestTree(t),
+			input{
+				0,
+			},
+			output{
+				"00000000000000021b6d2a8dca8d96e6dfa28a826037521bb587d3cb435c44c90139e87a7a4fa164",
+				nil,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tree, in, out := tc.tree, tc.in, tc.out
+
+			proof, err := tree.CreateMembershipProof(in.index)
+			if err != out.err {
+				t.Errorf("expected: %v, actual: %v", out.err, err)
+			}
+			if err == nil {
+				proofHex := hex.EncodeToString(proof)
+				if proofHex != out.proofHex {
+					t.Errorf("expected: %s, actual: %s", out.proofHex, proofHex)
 				}
 			}
 		})
